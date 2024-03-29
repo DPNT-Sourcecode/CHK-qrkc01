f71649ba408cb0e196a2f68a229aa9b041be1553
sbx_user1051 <sbx_user1051@169.254.36.21>
2024-03-29 15:15:34 +0000

    Fri Mar 29 15:15:34 UTC 2024

--- test/solution_tests/CHK_R1/test_round1.py
+++ test/solution_tests/CHK_R1/test_round1.py
@@ -5,3 +5,14 @@ class TestCheckoutSolution():
     def test_single_item(self):
         assert checkout("C") == 20
         assert checkout("D") == 15
+    
+    def test_mixed_no_offers(self):
+        assert checkout("ABC") == 100
+        assert checkout("DDDDCCC") == 120
+    
+    def test_empty_basket(self):
+        assert checkout("") == 0
+    
+    def test_special_offers(self):
+        assert checkout("AAAA") == 180
+        assert checkout("BB") == 45
