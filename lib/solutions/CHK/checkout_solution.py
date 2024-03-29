e882fd0180e1f72be05dd6c00b538d190d93c185
sbx_user1051 <sbx_user1051@169.254.36.21>
2024-03-29 15:09:34 +0000

    Fri Mar 29 15:09:34 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,4 +6,32 @@ def checkout(skus: str):
     """
     Calculate the total checkout value of all items in the basket
     considering any special offers
+
+    Args:
+    - skus (str): String containing the SKUs of all products
+
+    Returns:
+    - int: The total checkout value or -1 for illegal input
     """
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': '15'}
+    offers = {'A': (3,130), 'B': (2,45)}
+    items = {}
+    for sku in skus:
+        if sku not in prices:
+            return -1
+        if sku in items:
+            items[sku] += 1
+        else:
+            items[sku] = 1
+    
+    total_price = 0
+    for item, quantity in items.items():
+        if item in offers:
+            offer_quantity, offer_price = offers[item]
+            offer_count = quantity // offer_quantity
+            normal_count = quantity % offer_quantity
+            total_price += offer_count * offer_price + normal_count * prices[item]
+        else:
+            total_price += quantity * prices[item]    
+    return total_price    
+
