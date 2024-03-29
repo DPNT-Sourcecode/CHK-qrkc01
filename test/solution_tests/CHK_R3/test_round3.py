5f0d62b6888ff2c3f7db784b559bb18a6e1fc72f
sbx_user1051 <sbx_user1051@169.254.36.21>
2024-03-29 16:08:48 +0000

    Fri Mar 29 16:08:48 UTC 2024

---
+++ test/solution_tests/CHK_R3/test_round3.py
@@ -0,0 +1,22 @@
+from lib.solutions.CHK.checkout_solution import checkout
+
+
+class TestCheckoutNewSolution():
+    def test_discount_offers(self):
+        assert checkout("AAA") == 130
+        assert checkout("AAAAAA") == 250
+
+    def test_free_item(self):
+        assert checkout("EEB") == 80
+        assert checkout("EEEEBBB") == 190
+    
+    def test_illegal_input(self):
+        assert checkout("F") == -1
+    
+    def test_mixed_free_discount(self):
+        # 8A -> 2 offers 130 + 200
+        # EEB -> get B free 80
+        # BB -> offer 45 
+        # Total = 470
+        assert checkout("AAAAAEEBAAABB") == 455
+        assert checkout("AAAAAEEBAAAB") == 440
