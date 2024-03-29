3c3fa0ec7f5ade0dd19b8022bfafbd598125c310
sbx_user1051 <sbx_user1051@169.254.36.21>
2024-03-29 16:26:00 +0000

    Fri Mar 29 16:26:00 UTC 2024

--- test/solution_tests/CHK_R3/test_round3.py
+++ test/solution_tests/CHK_R3/test_round3.py
@@ -4,4 +4,8 @@ from lib.solutions.CHK.checkout_solution import checkout
 class TestCheckoutR3NewSolution():
     def test_new_F_offer(self):
         assert checkout("FFF") == 20
-        assert checkout("FFFFF") == 30
+        assert checkout("FFFFF") == 40
+
+    def test_mixed_F_offer(self):
+        assert checkout("FAFAAAAF") == 220
+        assert checkout("FBBFFFC") == 95
