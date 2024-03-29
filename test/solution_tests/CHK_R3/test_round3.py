bab6781ebe10160c929309f4e6f24592c72a12a1
sbx_user1051 <sbx_user1051@169.254.36.21>
2024-03-29 16:35:02 +0000

    Fri Mar 29 16:35:02 UTC 2024

--- test/solution_tests/CHK_R3/test_round3.py
+++ test/solution_tests/CHK_R3/test_round3.py
@@ -1,11 +1,11 @@
-from lib.solutions.CHK.checkout_solution import checkout
+from lib.solutions.CHK.checkout_solution import checkout_r3
 
 
 class TestCheckoutR3NewSolution():
     def test_new_F_offer(self):
-        assert checkout("FFF") == 20
-        assert checkout("FFFFF") == 40
+        assert checkout_r3("FFF") == 20
+        assert checkout_r3("FFFFF") == 40
 
     def test_mixed_F_offer(self):
-        assert checkout("FAFAAAAF") == 220
-        assert checkout("FBBFFFC") == 95
+        assert checkout_r3("FAFAAAAF") == 220
+        assert checkout_r3("FBBFFFC") == 95
